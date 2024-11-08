2a8060b05f745780cb01c415139fd42fc300daef
christoseteoglou <itsmeglue@gmail.com>
2024-11-08 11:15:59 +0100

    Initial Commit

---
+++ Meraki/MerakiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MerakiApp.swift
+//  Meraki
+//
+//  Created by Christos Eteoglou on 2024-11-08.
+//
+
+import SwiftUI
+
+@main
+struct MerakiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
